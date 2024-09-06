eb2491de7fa3f4e43e68766420aa6049772a551a
YASHTHAKUR2923 <tyash1864@gmail.com>
2024-09-06 22:02:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// You can add JavaScript functionality here.
+// For example, adding a simple greeting when the page loads:
+
+window.onload = () => {
+    console.log('Welcome to My Blog!');
+  };
+  
